bc72113e31731b8d5fcf2bd459241d3c009a83f1
stopyouhelper24 <101508503+stopyouhelper24@users.noreply.github.com>
2024-11-15 10:31:07 +0100

    Add files via upload

---
+++ Files_That_Are_Used/cmdpad.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class cmdpad
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(cmdpad))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WindowsCMDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SebsSWCV20CMDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewCMDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OldCMDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ShowToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Font = New System.Drawing.Font("Trebuchet MS", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingsToolStripMenuItem, Me.NewCMDToolStripMenuItem, Me.OldCMDToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 57)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1127, 37)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingsToolStripMenuItem1, Me.WindowsCMDToolStripMenuItem, Me.SebsSWCV20CMDToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.SettingsToolStripMenuItem.Font = New System.Drawing.Font("Trebuchet MS", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(65, 33)
+        Me.SettingsToolStripMenuItem.Text = "File"
+        '
+        'SettingsToolStripMenuItem1
+        '
+        Me.SettingsToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontToolStripMenuItem, Me.SToolStripMenuItem})
+        Me.SettingsToolStripMenuItem1.Name = "SettingsToolStripMenuItem1"
+        Me.SettingsToolStripMenuItem1.Size = New System.Drawing.Size(233, 34)
+        Me.SettingsToolStripMenuItem1.Text = "Settings"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(135, 34)
+        Me.FontToolStripMenuItem.Text = "Font"
+        '
+        'SToolStripMenuItem
+        '
+        Me.SToolStripMenuItem.Name = "SToolStripMenuItem"
+        Me.SToolStripMenuItem.Size = New System.Drawing.Size(135, 34)
+        Me.SToolStripMenuItem.Text = "s"
+        '
+        'WindowsCMDToolStripMenuItem
+        '
+        Me.WindowsCMDToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control
+        Me.WindowsCMDToolStripMenuItem.Name = "WindowsCMDToolStripMenuItem"
+        Me.WindowsCMDToolStripMenuItem.Size = New System.Drawing.Size(233, 34)
+        Me.WindowsCMDToolStripMenuItem.Text = "Windows CMD"
+        Me.WindowsCMDToolStripMenuItem.Visible = False
+        '
+        'SebsSWCV20CMDToolStripMenuItem
+        '
+        Me.SebsSWCV20CMDToolStripMenuItem.Name = "SebsSWCV20CMDToolStripMenuItem"
+        Me.SebsSWCV20CMDToolStripMenuItem.Size = New System.Drawing.Size(233, 34)
+        Me.SebsSWCV20CMDToolStripMenuItem.Text = "Old CMD"
+        Me.SebsSWCV20CMDToolStripMenuItem.Visible = False
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(230, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(233, 34)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'NewCMDToolStripMenuItem
+        '
+        Me.NewCMDToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem, Me.HideToolStripMenuItem})
+        Me.NewCMDToolStripMenuItem.Name = "NewCMDToolStripMenuItem"
+        Me.NewCMDToolStripMenuItem.Size = New System.Drawing.Size(124, 33)
+        Me.NewCMDToolStripMenuItem.Text = "New CMD"
+        '
+        'ShowToolStripMenuItem
+        '
+        Me.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        Me.ShowToolStripMenuItem.Size = New System.Drawing.Size(142, 34)
+        Me.ShowToolStripMenuItem.Text = "Show"
+        '
+        'HideToolStripMenuItem
+        '
+        Me.HideToolStripMenuItem.Name = "HideToolStripMenuItem"
+        Me.HideToolStripMenuItem.Size = New System.Drawing.Size(142, 34)
+        Me.HideToolStripMenuItem.Text = "Hide"
+        Me.HideToolStripMenuItem.Visible = False
+        '
+        'OldCMDToolStripMenuItem
+        '
+        Me.OldCMDToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem1, Me.HideToolStripMenuItem1})
+        Me.OldCMDToolStripMenuItem.Name = "OldCMDToolStripMenuItem"
+        Me.OldCMDToolStripMenuItem.Size = New System.Drawing.Size(114, 33)
+        Me.OldCMDToolStripMenuItem.Text = "Old CMD"
+        '
+        'ShowToolStripMenuItem1
+        '
+        Me.ShowToolStripMenuItem1.Name = "ShowToolStripMenuItem1"
+        Me.ShowToolStripMenuItem1.Size = New System.Drawing.Size(142, 34)
+        Me.ShowToolStripMenuItem1.Text = "Show"
+        '
+        'HideToolStripMenuItem1
+        '
+        Me.HideToolStripMenuItem1.Name = "HideToolStripMenuItem1"
+        Me.HideToolStripMenuItem1.Size = New System.Drawing.Size(142, 34)
+        Me.HideToolStripMenuItem1.Text = "Hide"
+        Me.HideToolStripMenuItem1.Visible = False
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(74, 33)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(149, 34)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox1.Font = New System.Drawing.Font("Trebuchet MS", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.White
+        Me.RichTextBox1.Location = New System.Drawing.Point(58, 155)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(947, 455)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = "C:/Sebs-SW-CV"
+        Me.RichTextBox1.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1127, 57)
+        Me.Panel1.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(958, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 48)
+        Me.Button3.TabIndex = 4
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(1070, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 48)
+        Me.Button1.TabIndex = 2
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(1015, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(49, 48)
+        Me.Button2.TabIndex = 3
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TabControl1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 94)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1127, 566)
+        Me.Panel2.TabIndex = 6
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1127, 566)
+        Me.TabControl1.TabIndex = 0
+        '
+        'FontDialog1
+        '
+        Me.FontDialog1.Font = New System.Drawing.Font("Trebuchet MS", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FontDialog1.ShowApply = True
+        Me.FontDialog1.ShowColor = True
+        '
+        'cmdpad
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1127, 660)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "cmdpad"
+        Me.Text = "cmdpad"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents SettingsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents WindowsCMDToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SebsSWCV20CMDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents SettingsToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewCMDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShowToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HideToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OldCMDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ShowToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents HideToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Files_That_Are_Used/cmdpad.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class cmdpad
+    Public opened As String = "0"
+    Dim mouse_move As System.Drawing.Point
+    Private Sub WindowsCMDToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles WindowsCMDToolStripMenuItem.Click
+        Process.Start("cmd.exe")
+    End Sub
+
+    Private Sub MenuStrip1_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MenuStrip1.MouseDown
+        'mouse_move = New Point(-e.X, -e.Y)
+    End Sub
+
+    Private Sub MenuStrip1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MenuStrip1.MouseMove
+        'If (e.Button = Windows.Forms.MouseButtons.Left) Then
+        'Dim mposition As Point
+        'mposition = Control.MousePosition
+        'mposition.Offset(mouse_move.X, mouse_move.Y)
+        'Me.Location = mposition
+        'If RichTextBox1.Text = "Friday" Then
+        ''SettingsToolStripMenuItem.DropDownItems
+        'Else
+        'SettingsToolStripMenuItem.HideDropDown()
+        'End If
+        'End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If WindowState = FormWindowState.Normal Then
+            WindowState = FormWindowState.Maximized
+        Else
+            WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub SebsSWCV20CMDToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SebsSWCV20CMDToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ShowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShowToolStripMenuItem.Click
+        Old_New_CMDTab = "N"
+        tools()
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112, &HF012, 0)
+    End Sub
+    'Drag Form
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+    Public S As New UserControl1
+    Public S2 As New UserControl2
+    Private Sub cmdpad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Button1.BackgroundImage = CVW_res.close1.Image
+        Button2.BackgroundImage = CVW_res.maxi.Image
+        Button3.BackgroundImage = CVW_res.mini1.Image
+        Me.Panel2.Controls.Add(S)
+        Me.Panel2.Controls.Add(S2)
+        S.Dock = DockStyle.Fill
+        S2.Dock = DockStyle.Fill
+        S.Hide()
+        S2.Hide()
+        Old_New_CMDTab = "N"
+        tools()
+
+
+        Panel1.BackColor = Color.Silver
+
+    End Sub
+
+    Private Sub HideToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HideToolStripMenuItem.Click
+        'S.Hide()
+    End Sub
+
+    Private Sub ShowToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ShowToolStripMenuItem1.Click
+        Old_New_CMDTab = "O"
+        tools()
+    End Sub
+
+    Private Sub HideToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles HideToolStripMenuItem1.Click
+        'S2.Hide()
+    End Sub
+    Public tabnum As Integer = 1
+    Public Old_New_CMDTab As String = "N"
+    Public Sub tools()
+        If tabnum = 11 Then
+        Else
+
+            Dim ss As New TabPage
+            Dim WEBB As New UserControl1
+            Dim WEBB2 As New UserControl2
+            With ss
+                .Text = "TAB " & tabnum
+                '.Controls.Add(WEBB)
+                If Old_New_CMDTab = "N" Then
+                    .Controls.Add(WEBB)
+                    WEBB.Dock = DockStyle.Fill
+                    WEBB.RichTextBox1.Font = S22
+                ElseIf Old_New_CMDTab = "O" Then
+                    .Controls.Add(WEBB2)
+                    WEBB2.Dock = DockStyle.Fill
+                    WEBB2.RichTextBox1.Font = S22
+                End If
+
+
+            End With
+
+
+            TabControl1.Controls.Add(ss)
+            tabnum = tabnum + 1
+        End If
+        'MsgBox(tabnum.ToString)
+    End Sub
+    Public S22 As Font
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        If FontDialog1.ShowDialog = DialogResult.OK Then
+            S22 = FontDialog1.Font
+        End If
+    End Sub
+
+    Private Sub FontDialog1_Apply(sender As Object, e As EventArgs) Handles FontDialog1.Apply
+
+    End Sub
+
+    Private Sub cmdpad_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        Form15.Show()
+        About2.Show()
+        About2.LabelProductName.Text = "Product CMD"
+        About2.LabelVersion.Text = "Version 2.5.0.1"
+
+        'About2.PictureBox1.Image = Form_1pad.Button11.BackgroundImage
+        About2.PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        About2.LabelCopyright.Text = My.Application.Info.Copyright
+        About2.LabelCompanyName.Text = My.Application.Info.CompanyName
+        About2.TextBoxDescription.Text = "Description:
+CMD can open Forms, Apps and install Apps.
+
+You Can use the New or Old CMD Commands."
+        Form15.Close()
+    End Sub
+End Class
